5567f6512673244305f8e5c11074c221e18e2906
gutsserrano <gutsserrano@gmail.com>
2024-04-18 16:14:20 -0300

    Add project files.

---
+++ NumeroInicialEFinal/Program.cs
@@ -0,0 +1,38 @@
+﻿// Faça um programa que leia um numero inicial e um numero final, mostrando entre eles, quais sao os valores pares e impares
+
+int inicial = 0, final = 0;
+int aux;
+
+Console.WriteLine("Digite o número inicial: ");
+inicial = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o número final: ");
+final = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\nPares: ");
+aux = inicial;
+aux++;
+while (aux < final){
+    
+    if (aux % 2 == 0)
+    {
+        Console.WriteLine(aux);
+    }
+    aux++;
+}
+
+Console.WriteLine("\nImpares: ");
+aux = inicial;
+aux++;
+while (aux < final)
+{
+
+    if (aux % 2 != 0)
+    {
+        Console.WriteLine(aux);
+    }
+    aux++;
+}
+
+Console.WriteLine("\nDigite qualquer tecla para sair...");
+Console.ReadKey();
